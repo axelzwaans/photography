207198ca7f2762f4deedbda9f9179b359731169c
Axel Zwaans <axelzwaans@hotmail.com>
2024-02-17 18:50:45 +0000

    Initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,53 @@
-import React from 'react';
+// import components
+import Socials from "./Socials";
+import Logo from "../img/header/logo.svg";
+import MobileNav from "./MobileNav";
+
+// import link
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[130px] flex items-center">
+      <div className="flex flex-col lg:flex-row lg:items-center w-full justify-between">
+        {/* Logo */}
+        <Link to={"/"} className="max-w-[200px]">
+          <img src={Logo} alt="logo" />
+        </Link>
+        {/* Navigation - initially hidden - show on desktop mode */}
+        <nav className="hidden lg:flex gap-x-12 font-semibold">
+          <Link
+            to={"/"}
+            className="text-[#696c6d] hover:text-primary transition"
+          >
+            Home
+          </Link>
+          <Link
+            to={"/about"}
+            className="text-[#696c6d] hover:text-primary transition"
+          >
+            About
+          </Link>
+          <Link
+            to={"/portfolio"}
+            className="text-[#696c6d] hover:text-primary transition"
+          >
+            Portfolio
+          </Link>
+          <Link
+            to={"/contact"}
+            className="text-[#696c6d] hover:text-primary transition"
+          >
+            Contact
+          </Link>
+        </nav>
+      </div>
+      {/* Socials */}
+      <Socials />
+      {/* Mobile Navigation */}
+      <MobileNav />
+    </header>
+  );
 };
 
 export default Header;
