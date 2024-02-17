69394fa2c14aaf70dabea1d010b931f3fa324d21
Axel Zwaans <axelzwaans@hotmail.com>
2024-02-17 19:00:52 +0000

    Complete mobile navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
           <img src={Logo} alt="logo" />
         </Link>
         {/* Navigation - initially hidden - show on desktop mode */}
-        <nav className="hidden lg:flex gap-x-12 font-semibold">
+        <nav className="hidden xl:flex gap-x-12 font-semibold">
           <Link
             to={"/"}
             className="text-[#696c6d] hover:text-primary transition"
--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -10,17 +10,43 @@ import { Link } from "react-router-dom";
 //import motion
 import { motion } from "framer-motion";
 
+// menu variants
+const menuVariants = {
+  hidden: {
+    x: "100%",
+  },
+  show: {
+    x: 0,
+    transition: {
+      ease: [0.6, 0.01, -0.05, 0.9],
+    },
+  },
+};
+
 const MobileNav = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
-    <nav>
+    <nav className="text-primary xl:hidden">
       {/* nav open button */}
-      <div className="text-3xl cursor-pointer">
+      <div
+        onClick={() => setOpenMenu(true)}
+        className="text-3xl cursor-pointer"
+      >
         <CgMenuRight />
       </div>
       {/* menu */}
-      <div className="bg-white shadow-2xl w-full absolute top-0 right-0 max-w-xs h-screen z-20">
+      <motion.div
+        variants={menuVariants}
+        initial="hidden"
+        animate={openMenu ? "show" : ""}
+        className="bg-white shadow-2xl w-full absolute top-0 right-0 max-w-xs h-screen z-20"
+      >
         {/* icon */}
-        <div className="text-4xl absolute z-30 left-4 top-14 text-primary cursor-pointer">
+        <div
+          onClick={() => setOpenMenu(false)}
+          className="text-4xl absolute z-30 left-4 top-14 text-primary cursor-pointer"
+        >
           <IoMdClose />
         </div>
         {/* menu list */}
@@ -38,7 +64,7 @@ const MobileNav = () => {
             <Link to="/contact">Contact</Link>
           </li>
         </ul>
-      </div>
+      </motion.div>
     </nav>
   );
 };
