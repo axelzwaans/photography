1e5179b0f34e4bb0cd2b55e044088e19a4446fc0
Axel Zwaans <axelzwaans@hotmail.com>
2024-02-21 13:38:43 +0000

    Edit main title

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -36,7 +36,7 @@ const Home = () => {
             className="w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start"
           >
             <h1 className="h1">
-              photographer <br /> & Tree Planter
+              photographer <br /> & Film Maker
             </h1>
             <p className="text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12">
               Moray, Scotland
